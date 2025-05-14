0cb450ea5f0bb1c5a0f2a3ff4648c358a6c05dcc
riccardo-guido <guidoriccardo02@gmail.com>
2025-05-14 19:14:00 +0200

    Milestone-2

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,18 +1,19 @@
-const posts = require("../data/db");
+const { posts } = require("../data/db");
 
 const index = (req, res) => {
     res.json({
-        data: posts.posts,
+        data: posts,
         status: 200
     });  
 }
 
 const show = (req, res) => {
-     const id = req.params.id;
-  
+  const id = parseInt(req.params.id);
+  const post = posts.find(currentPost => currentPost.id === id);
+ 
   res.json({
     description: "Lista del dettaglio del post " + id,
-    data: posts.posts,
+    data: post,
   });
 };
 
@@ -31,8 +32,27 @@ const modify = (req, res) => {
 };
 
 const destroy = (req, res) => {
-  const id = req.params.id;
-  res.json("Eliminazione del post " + id);
+    const id = parseInt(req.params.id);
+  const post = posts.find(currentPost => currentPost.id === id);
+
+  if(!post) {
+    res.status(404);
+
+    res.json({
+        error: '404 Not Found',
+        message: 'Post not found'
+    });
+
+    return;
+  }
+
+  const postIndex = posts.indexOf(post);
+  posts.splice(postIndex, 1);
+
+  console.log(posts);
+  
+  res.sendStatus(204);
+
 };
 
 
